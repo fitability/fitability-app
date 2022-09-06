dc45cb0e8be398ff827eb935246160a93d9c4b30
Justin Yoo <justin.yoojh@gmail.com>
2022-09-07 08:52:41 +0900

    Add Initial Blazor WASM app

---
+++ src/Fitability.Web/Models/UserClaimModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Fitability.Web.Models
+{
+    /// <summary>
+    /// This represents the model entity for authenticated user's claim details.
+    /// </summary>
+    public class UserClaimModel
+    {
+        /// <summary>
+        /// Gets or sets the issuer.
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the original issuer.
+        /// </summary>
+        public string OriginalIssuer { get; set; }
+
+        /// <summary>
+        /// Gets or sets the claim type.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the claim value.
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ src/Fitability.Web/Program.cs
@@ -0,0 +1,20 @@
+using Fitability.Web;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+});
+
+await builder.Build().RunAsync();
